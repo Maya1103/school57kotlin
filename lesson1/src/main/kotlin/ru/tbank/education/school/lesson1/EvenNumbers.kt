be96b22ad10302df0170ccb05ac6af0d2dd596aa
Maya1103 <maya.zapunidi@gmail.com>
2025-09-12 16:56:33 +0300

    задача

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
@@ -1,8 +1,19 @@
 package ru.tbank.education.school.lesson1
 
-/**
- * Сумма четных чисел.
- */
+fun main() {
+    val numbers = arrayOf(1,2,3,4,5,6,7,8,9)
+    print(sumEvenNumbers(numbers))
+}
+
+
 fun sumEvenNumbers(numbers: Array<Int>): Int {
-    TODO()
+    var n = 0
+    for(i in numbers)
+    {
+        if(i%2==0)
+        {
+            n = n + i
+        }
+    }
+    return n
 }
